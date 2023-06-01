994d1658df8671dc135ff18578ce48356b4dc9c9
Myprofilee <andrep13.id@gmail.com>
2023-06-01 09:26:35 +0700

    first commit

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -3,52 +3,51 @@ link.rel = 'icon';
 link.href = '/assets/img/icons/koraa.png';
 document.head.appendChild(link);
 
-var link = document.createElement('link');
-link.rel = 'canonical';
-link.href = 'https://admin.koraa.my.id/assets/img/koraa.png';
-document.head.appendChild(link);
+var linkCanonical = document.createElement('link');
+linkCanonical.rel = 'canonical';
+linkCanonical.href = 'https://admin.koraa.my.id/assets/img/koraa.png';
+document.head.appendChild(linkCanonical);
 
 
 
-var meta = document.createElement('meta');
-meta.name = 'description';
-meta.content = 'Koraa merupakan salah satu e-commerce di Indonesia yang menawarkan berbagai macam source code, design, dll';
-document.head.appendChild(meta);
+var metaDescription = document.createElement('meta');
+metaDescription.setAttribute('name', 'description');
+metaDescription.content = 'Koraa merupakan salah satu e-commerce di Indonesia yang menawarkan berbagai macam source code, design, dll';
+document.head.appendChild(metaDescription);
 
-var meta = document.createElement('meta');
-meta.name = 'keywords';
-meta.content = 'admin koraa, koraa';
-document.head.appendChild(meta);
-
-
+var metaKeywords = document.createElement('meta');
+metaKeywords.setAttribute('name', 'keywords');
+metaKeywords.content = 'admin koraa, koraa';
+document.head.appendChild(metaKeywords);
 
 
-var meta = document.createElement('meta');
-meta.name = 'og:url';
-meta.content = 'https://admin.koraa.my.id/';
-document.head.appendChild(meta);
 
-var meta = document.createElement('meta');
-meta.property = 'og:title';
-meta.content = 'Admin | Koraa';
-document.head.appendChild(meta);
+var metaUrl = document.createElement('meta');
+metaUrl.setAttribute('property', 'og:url');
+metaUrl.content = 'https://admin.koraa.my.id/';
+document.head.appendChild(metaUrl);
 
-var meta = document.createElement('meta');
-meta.property = 'og:description';
-meta.content = 'Koraa merupakan salah satu e-commerce di Indonesia yang menawarkan berbagai macam source code, design, dll';
-document.head.appendChild(meta);
+var metaTitle = document.createElement('meta');
+metaTitle.setAttribute('property', 'og:title');
+metaTitle.content = 'Admin | Koraa';
+document.head.appendChild(metaTitle);
 
-var meta = document.createElement('meta');
-meta.property = 'og:image';
-meta.content = 'https://admin.koraa.my.id/assets/img/koraa.png';
-document.head.appendChild(meta);
+var metaDescriptionOG = document.createElement('meta');
+metaDescriptionOG.setAttribute('property', 'og:description');
+metaDescriptionOG.content = 'Koraa merupakan salah satu e-commerce di Indonesia yang menawarkan berbagai macam source code, design, dll';
+document.head.appendChild(metaDescriptionOG);
 
-var meta = document.createElement('meta');
-meta.property = 'og:site_name';
-meta.content = 'admin koraa';
-document.head.appendChild(meta);
+var metaImage = document.createElement('meta');
+metaImage.setAttribute('property', 'og:image');
+metaImage.content = 'https://admin.koraa.my.id/assets/img/koraa.png';
+document.head.appendChild(metaImage);
 
-var meta = document.createElement('meta');
-meta.property = 'og:ttl';
-meta.content = '3600"';
-document.head.appendChild(meta);
+var metaSiteName = document.createElement('meta');
+metaSiteName.setAttribute('property', 'og:site_name');
+metaSiteName.content = 'admin koraa';
+document.head.appendChild(metaSiteName);
+
+var metaTtl = document.createElement('meta');
+metaTtl.setAttribute('property', 'og:ttl');
+metaTtl.content = '3600';
+document.head.appendChild(metaTtl);
