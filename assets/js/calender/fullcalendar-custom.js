4cb0feb736e33becb3cd6c9e3ca0e395bedfbfc4
Myprofilee <andrep13.id@gmail.com>
2023-06-02 15:44:42 +0700

    first commit

--- assets/js/calender/fullcalendar-custom.js
+++ assets/js/calender/fullcalendar-custom.js
@@ -32,6 +32,11 @@ document.addEventListener('DOMContentLoaded', function() {
           start: '2023-05-31'
         },
 
+        {
+          title: 'Tidak Dapat Di Akses',
+          start: '2023-06-02'
+        },
+
         /* Koraa
         {
           title: 'Reporting about Theme',
