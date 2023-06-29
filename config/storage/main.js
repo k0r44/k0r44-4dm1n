9cc4282603ad3539fdefb628eafabfe5d19a003c
Myprofilee <andrep13.id@gmail.com>
2023-06-29 14:39:12 +0700

    first commit

--- config/storage/main.js
+++ config/storage/main.js
@@ -1,6 +1,6 @@
 function getPlanStorageLimit(plan) {
     if (plan === 'free') {
-      return '1 GB';
+      return '1 MB';
     } else if (plan === 'silver') {
       return '200 GB';
     } else {
