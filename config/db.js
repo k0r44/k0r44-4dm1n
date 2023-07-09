017675e9aca71554c7ff3996eb6c47e0dc934a51
Myprofilee <andrep13.id@gmail.com>
2023-07-09 11:38:07 +0700

    first commit

--- config/db.js
+++ config/db.js
@@ -3,18 +3,11 @@ firebase.initializeApp(firebaseConfig);
       
 // Memeriksa status login pengguna saat ini
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    var username = user.displayName; // Menggunakan displayName pengguna sebagai username
-
-    // Memeriksa apakah email pengguna sudah diverifikasi
-    if (user.emailVerified) {
+    if (user) {
+      var username = user.displayName; // Menggunakan displayName pengguna sebagai username
       getFileInfo(username);
     } else {
-      console.log('Email pengguna belum diverifikasi');
-      window.location.href = '/verify-email'; // Mengarahkan pengguna ke halaman verifikasi email
+      console.log('Pengguna belum login');
+      window.location.href = '/auth/login/'; // Mengarahkan pengguna ke halaman login
     }
-  } else {
-    console.log('Pengguna belum login');
-    window.location.href = '/auth/login/'; // Mengarahkan pengguna ke halaman login
-  }
-});
+  });
