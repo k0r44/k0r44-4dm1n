1b85c557151518ebf06b9b418404b47f8a946616
Myprofilee <andrep13.id@gmail.com>
2023-07-18 09:41:23 +0700

    first commit

---
+++ config/users/verif.js
@@ -0,0 +1,13 @@
+firebase.initializeApp(firebaseConfig);
+
+// Mendapatkan referensi objek firebase.auth()
+var auth = firebase.auth();
+
+// Mendeteksi perubahan status otentikasi pengguna
+auth.onAuthStateChanged(function(user) {
+  // Memeriksa apakah pengguna telah masuk dan email sudah diverifikasi
+  if (user && user.emailVerified) {
+    // Redirect pengguna ke halaman tujuan
+    window.location.href = "/";
+  }
+});
